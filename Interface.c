9f6c1543f149a84adfb2b51e8550151e00531b86
Joao5Rodrigues <96350298+Joao5Rodrigues@users.noreply.github.com>
2022-10-04 11:39:41 +0100

    pitagoras double check

--- Interface.c
+++ Interface.c
@@ -31,28 +31,32 @@ UINT32 do_math(OPERATION oper, UINT32 input_1, UINT32 input_2, UINT8 * valid){
 }
 
 INT32 pitagoras(UINT16 cateto_1, UINT16 cateto_2){
-    BOOLEAN valid_flag;
+    BOOLEAN valid_flag = TRUE;
+    BOOLEAN check_failing = TRUE;
     INT32 output;
+
     if (cateto_1>=255 || cateto_2>=255){
-        return -1;
+        check_failing = FALSE;
     }else if(sqrt(pow(cateto_1,2)+pow(cateto_2,2))==0){
-        return -1;
+        check_failing = FALSE;
     }else
 
     cateto_1 = multiplication(cateto_1, cateto_1, &valid_flag);
     if (valid_flag == FALSE){
-        return -1;
+        check_failing = FALSE;
     }
 
     cateto_2 = multiplication(cateto_2, cateto_2, &valid_flag);
     if (valid_flag == FALSE){
-        return -1;
+        check_failing = FALSE;
     }
 
     output = sum(cateto_1, cateto_2, valid_flag);
     if (valid_flag == FALSE){
-        return -1;
+        check_failing = FALSE;
     }
 
-    return output;//just C or C^2 
+    if(check_failing == TRUE){
+        return output;//just C or C^2 
+    }else return -1;
 }
